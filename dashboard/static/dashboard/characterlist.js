1afe98a8cc8016664d6a5b11e2edf5fb1385da4e
Florian Tautz <dev@nyronet.de>
2018-05-22 23:34:29 +0200

    only update tile if request was successful

--- dashboard/static/dashboard/characterlist.js
+++ dashboard/static/dashboard/characterlist.js
@@ -1,14 +1,12 @@
-function tileLoad() {
-    console.log("tile loaded: " + this.responseText);
-}
-
 function loadCharacterDetails() {
     const tiles = document.getElementsByClassName("character");
     for (let i = 0; i < tiles.length; i++) {
         let tile = tiles[i];
         let request = new XMLHttpRequest();
         request.addEventListener("load", function() {
-            tile.outerHTML = this.responseText;
+            if (this.status === 200) {
+                tile.outerHTML = this.responseText;
+            }
         });
         request.open("GET", "/dashboard/character/" + tile.dataset.characterid + "/", true);
         request.send();
